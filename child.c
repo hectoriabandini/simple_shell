f7402b8eadd087cc2dd950daa8f1c0934e64fc4b
hectoriabandini <abuhmoseji@gmail.com>
2023-05-22 13:05:47 +0000

    child call

--- child.c
+++ child.c
@@ -1,32 +1,42 @@
 #include "ourshell.h"
+/**
+ * child_exec - A function to engage a new process
+ * @commands: A pointer to a string of commands
+ * Return: Always success
+ */
 int child_exec(char **commands)
 {
-        //Path to binary file to be executed
-        char *bin;
-        //Status of child process
-        int status;
-        //Child process
-        pid_t pid, wpid;
-        bin = commands[0];
-        //Create child
-        pid = fork();
-        //externals_boiler(commands);
-        if(pid == 0)
-        {
-                if (execvp(bin, commands) == -1)
-                {
-                        perror("ERROR!");
-                };
-                        exit(EXIT_FAILURE);
-        } else if(pid < 0)
-        {
-                printf("Child creation failed");
-                exit(EXIT_FAILURE);
-        }
-        else
-        {
-                do {
-                        wpid = waitpid(pid, &status, WUNTRACED);
-                } while (!WIFEXITED(status) && !WIFSIGNALED(status));
-        }
+	/**Path to binary file to be executed**/
+	char *bin;
+	/**Status of child process**/
+	int status;
+	/**Child process**/
+
+	pid_t pid, wpid
+		;
+	bin = commands[0];
+
+	/**Create child**/
+	pid = fork();
+	/**externals_boiler(commands);**/
+	if (pid == 0)
+	{
+		if (execvp(bin, commands) == -1)
+		{
+			perror("ERROR!");
+		};
+		exit(EXIT_FAILURE);
+	}
+	else if (pid < 0)
+	{
+		printf("Child creation failed");
+		exit(EXIT_FAILURE);
+	}
+	else
+	{
+		do {
+			wpid = waitpid(pid, &status, WUNTRACED);
+		} while (!WIFEXITED(status) && !WIFSIGNALED(status));
+	}
+	return (1)
 }
