d2c1cb8c079345c9ca2bdb9511c3a7456febdf34
NamwambaMarvin <marvinnamwamba@gmail.com>
2023-05-25 05:14:11 +0300

    push

---
+++ simple.c
@@ -0,0 +1,28 @@
+#include "simple.h"
+#include <stdlib.h>
+#include <stdio.h>
+#include <unistd.h>
+#include <sys/wait.h>
+
+int main(int argc, char *argv[])
+{
+	char *path = "/bin/sh";
+	pid_t child;
+	int status;
+	char *myfile = argv[0];
+	char **env_list = environ;
+	char *commands[] = {myfile, "-s", NULL};
+	if (argc == 0)
+	{
+		exit(0);
+	}
+	child = fork();
+	if (child == 0)
+	{
+		execve(path, commands, env_list);
+	} else
+	{
+		wait(&status);
+	}
+	return (0);
+}
